6e32bbbda03cff24cdc076bb95eb9b68a61b95f5
Eslam Kamal <ek191744@gmail.com>
2025-05-21 10:25:41 +0300

    Initialize Server Side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,26 @@
+import {
+    SignedIn,
+    SignedOut,
+    SignInButton,
+    UserButton,
+} from "@clerk/clerk-react";
+import { Button } from "./components/ui/button";
+
 function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <header>
+                <SignedOut>
+                    <SignInButton>
+                        <Button>
+                            Sign In
+                        </Button>
+                    </SignInButton >
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </header>
         </>
     );
 }
