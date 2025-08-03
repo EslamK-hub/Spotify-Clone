5a4750da0e0f44f1ccee84488aef7137a228bd13
Eslam Kamal <ek191744@gmail.com>
2025-08-03 08:00:57 +0300

    Finish Admin Controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,23 +1,29 @@
 import User from "../models/user.model.js";
 
-export const authCallback = async (req, res) => {
+export const authCallback = async (req, res, next) => {
     try {
         const { id, firstName, lastName, imageUrl } = req.body;
 
         const user = await User.findOne({ clerkId: id });
-        
-        if (!user) { 
+
+        if (!user) {
             await User.create({
                 clerkId: id,
                 fullName: `${firstName} ${lastName}`,
                 imageUrl: imageUrl || "",
-            })
+            });
         }
 
-        res.status(200).json({ success: true, message: "User authenticated successfully" });
-    } catch (error) { 
+        res.status(200).json({
+            success: true,
+            message: "User authenticated successfully",
+        });
+    } catch (error) {
         console.error("Error in auth callback:", error);
-        res.status(500).json({ success: false, message: "Internal Server Error" });
-        next(error)
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+        });
+        next(error);
     }
 };
