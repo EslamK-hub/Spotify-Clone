1adafd268eb8e83587bb9eb2037c78462ad6693f
Eslam Kamal <ek191744@gmail.com>
2025-08-19 10:08:13 +0300

    Create User Routes and Controller

---
+++ server/controllers/stat.controller.js
@@ -0,0 +1,31 @@
+export const getStats = async (req, res, next) => {
+    try {
+        const [totalSongs, totalUsers, totalAlbums, uniqueArtists] =
+            await Promise.all([
+                Song.countDocuments(),
+                User.countDocuments(),
+                Album.countDocuments(),
+                Song.aggregate([
+                    {
+                        $unionWith: {
+                            coll: "albums",
+                            pipeline: [],
+                        },
+                        $group: {
+                            _id: "$artist",
+                        },
+                        $count: "count",
+                    },
+                ]),
+            ]);
+
+        res.status(200).json({
+            totalSongs,
+            totalUsers,
+            totalAlbums,
+            uniqueArtists: uniqueArtists[0]?.count || 0,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
